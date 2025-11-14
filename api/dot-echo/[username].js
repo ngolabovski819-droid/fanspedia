b1fff96a3394e4c6b622fc31ed36bdf1cc8af94e
Nick <ngolabovski819@gmail.com>
2025-11-14 22:17:51 +0100

    fix: remove non-essential API functions to stay under Hobby plan 12-function limit

--- api/dot-echo/[username].js
+++
@@ -1,7 +0,0 @@
-export default function handler(req, res) {
-  let u = req.query.username;
-  if (Array.isArray(u)) u = u.join('.');
-  res.setHeader('Content-Type','application/json');
-  res.setHeader('X-Dot-Echo','1');
-  res.status(200).json({ ok: true, username: u, path: req.url });
-}
